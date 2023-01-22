0a6aa671ce04afed70d9228e8229d1d5009b55d6
MorozovMike <morozov.mishka@gmail.com>
2023-01-22 16:03:06 +0300

    Init generating news template function and function for render this template.

--- app.js
+++ app.js
@@ -88,5 +88,38 @@ function loadNews() {
 
 // On get response from server function
 function onGetResponse(err, res) {
-  console.log(res);
+  renderNews(res.articles);
+}
+
+// Render news
+function renderNews(news) {
+  const newsContainer = document.querySelector(".news-container .row");
+  let fragment = '';
+
+  news.forEach(newsItem => {
+    const el = newsTemplate(newsItem);
+    fragment += el;
+  });
+
+  newsContainer.insertAdjacentHTML('afterbegin', fragment);
+}
+
+// News item templeate
+function newsTemplate({ urlToImage, title, url, description}) {
+  return `
+    <div class="col s12">
+        <div class="card">
+            <div class="card-image">
+                <img src="${urlToImage}">
+                <span class="card-title">${title || ''}</span>
+            </div>
+            <div class="card-content">
+                <p>${description || ''}</p>
+            </div>
+            <div class="card-action">
+                <a href="${url}">Read more</a>
+            </div>
+        </div>
+    </div>
+  `;
 }
