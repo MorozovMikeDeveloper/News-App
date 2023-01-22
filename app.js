7f6f02d0bc5037f703e5120c23cadd2172e98c96
MorozovMike <morozov.mishka@gmail.com>
2023-01-22 15:19:19 +0300

    Markup. Init search container with select and text input.

---
+++ app.js
@@ -0,0 +1,63 @@
+function customHttp() {
+  return {
+    get(url, cb) {
+      try {
+        const xhr = new XMLHttpRequest();
+        xhr.open("GET", url);
+
+        xhr.addEventListener("load", () => {
+          if (Math.floor(xhr.status / 100) !== 2) {
+            cb(`Error. Status code: ${xhr.status}`, xhr);
+            return;
+          }
+          const response = JSON.parse(xhr.responseText);
+          cb(null, response);
+        });
+
+        xhr.addEventListener("error", () => {
+          cb(`Error. Status code: ${xhr.status}`, xhr);
+        });
+
+        xhr.send();
+      } catch (error) {
+        cb(error);
+      }
+    },
+    post(url, body, headers, cb) {
+      try {
+        const xhr = new XMLHttpRequest();
+        xhr.open("POST", url);
+
+        xhr.addEventListener("load", () => {
+          if (Math.floor(xhr.status / 100) !== 2) {
+            cb(`Error. Status code: ${xhr.status}`, xhr);
+            return;
+          }
+          const response = JSON.parse(xhr.responseText);
+          cb(null, response);
+        });
+
+        xhr.addEventListener("error", () => {
+          console.log("error");
+        });
+
+        if (headers) {
+          Object.entries(headers).forEach(([key, value]) => {
+            xhr.setRequestHeader(key, value);
+          });
+        }
+
+        xhr.send();
+      } catch (error) {
+        cb(error);
+      }
+    },
+  };
+}
+
+let http = customHttp();
+
+// init selects
+document.addEventListener("DOMContentLoaded", () => {
+  M.AutoInit();
+});
